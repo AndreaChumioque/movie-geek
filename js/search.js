e8eef17f03004fba1245bf395a72d24b92ff680f
andrea-isabel <andrea19.93.15@gmail.com>
2018-02-21 05:59:15 -0500

    Revert "Agregando cambios"
    This reverts commit 910aebd4c3b731e5c37ec7cdf62dfe423a13c59f.

--- js/search.js
+++ js/search.js
@@ -1,25 +1,21 @@
 $(document).ready(function() {
   // Declarando variables
   var inputFld = $('#titleFld');
-  var $searchBtn = $('#searchBtn');
+  var searchBtn = $('#searchBtn');
   var movieList = $('#movieList');
-
-  const apiKey = '104b6b3edad3ec946caa9bae8940699c';
-
   var arrResults = [];
 
   var $movie = $('.movie');
 
   // Asociando eventos
-  $searchBtn.click(handleSearchBtn);
+  searchBtn.click(handleSearchBtn);
   $(document).on('click', $movie, getDetails);
 
   // Función para hacer la busqueda general
   function handleSearchBtn() {
-    let title = inputFld.val();
-    // console.log(title);
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURI(title)}`;
-
+    var title = inputFld.val();
+    console.log(title);
+    var url = 'https://www.omdbapi.com/?&apikey=5bb8748d&s=' + encodeURI(title) + '&type=movie';
     $.ajax({
       url: url,
       success: renderMovies,
@@ -29,39 +25,49 @@ $(document).ready(function() {
   // Función para mostrar filtrando por genero Sci-fi, Adventure y/o Fantasy
   function renderMovies(response) {
     $('#movieList').empty();
-    var data = response.results;
+    arrResults = [];
 
-    for (const movie of data) {
-      if (movie.genre_ids.includes(28) || movie.genre_ids.includes(878)) {
-        console.log(movie);
-        let image;
+    console.log(response);
 
-        if (movie.poster_path) {
-          image = `<div class="col-4">
-          <img class="img-fluid" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
-        </div>`;
-        } else {
-          image = `<div class="col-4">
-          <img class="img-fluid" src="assets/images/not-picture.svg">
-        </div>`;
-        }
+    for (var i in response.Search) {
+      var movie = response.Search[i];
+      var idmovie = movie.imdbID;
+      arrResults.push(idmovie);
+    }
 
-        let info = `<div class="col-8 info">
-                      <h4>${movie.title}</h4>
-                      <p><i class="fas fa-users"></i> ${movie.popularity.toFixed(1)}%</p>
-                      <p><i class="fas fa-star"></i> ${movie.vote_average}</p>
-                      <p class="d-none d-sm-block"><i class="fas fa-file-alt"></i> ${movie.overview}</p>
-                    </div>`;
+    console.log(arrResults);
 
-        let structure = `<li class="d-block mt-3 p-2 box-shadow col-10 offset-1 col-md-8 offset-md-2 movie" id="${movie.original_title}">
-                          <div class="row">
-                          ${image}
-                          ${info}
-                          </div>
-                        </li>`;
+    for (var i = 0; i < arrResults.length; i++) {
+      $.getJSON('https://www.omdbapi.com/?&apikey=3a181f1c&i=' + arrResults[i])
+        .then(function(response) {
+          if ((response.Genre.indexOf('Sci-Fi') !== -1 || response.Genre.indexOf('Adventure') !== -1 || response.Genre.indexOf('Fantasy') !== -1) && response.Genre.indexOf('Animation') === -1) {
+            console.log(response);
+            var li = $('<li class="d-block mt-3 p-2 box-shadow col-10 offset-1 col-md-8 offset-md-2 movie" id="' + response.imdbID + '"></li>');
+            var movieRow = $('<div class="row"></div>');
+            var img = '';
+            if (response.Poster !== 'N/A') {
+              img = $('<div class="col-4"><img class="img-fluid" src="' + response.Poster + '"></div>');
+            } else {
+              img = $('<div class="col-4"><img class="img-fluid" src="../assets/images/not-picture.svg"></div>');
+            }
+            var info = $('<div class="col-8 info"></div>');
+            var title = $('<h4>' + response.Title + '</h4>');
+            var runtime = $('<p class="d-none d-sm-block">Runtime: ' + response.Runtime + '</p>');
+            var director = $('<p>Director: ' + response.Director + '</p>');
+            var tag = $('<p class="d-none d-sm-block">Genre: ' + response.Genre + '</p>');
 
-        $('#movieList').append(structure);
-      }
+            li.append(movieRow);
+            movieRow.append(img);
+            movieRow.append(info);
+            info.append(title);
+            info.append(runtime);
+            info.append(director);
+            info.append(tag);
+            movieList.append(li);
+          } else if (arrResults.length === 0) {
+            movieList.append('li').addClass('p-3').text('Oops... we did not find that. Try again');
+          }
+        });
     }
   }
 
